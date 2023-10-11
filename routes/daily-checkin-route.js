04a79f01b7a8e0e07731c1014b1a1ac719417a5d
shivamsherje <sherjes49@gmail.com>
2023-10-11 11:05:55 +0530

    Improved:res.status massage

--- routes/daily-checkin-route.js
+++ routes/daily-checkin-route.js
@@ -7,18 +7,18 @@ dailyCheckinRoute.post('/post', async (req, res) => {
     try {
         const newCheckin = new dailyCheckinModel(req.body);
         const saveDailyCheckin = await newCheckin.save();
-        res.status(201).json({ message: "Boat created successfully", data: saveDailyCheckin, status: true });
+        res.status(201).json({ message: "dailyCheckin data posted successfully", data: saveDailyCheckin, status: true });
     } catch (error) {
-        res.status(500).json({ error: 'Could not create a new boat', status: false });
+        res.status(500).json({ error: 'Could not post dailyCheckin data', status: false });
     }
 });
 
 dailyCheckinRoute.get('/', async (req, res) => {
     try {
         const Checkins = await dailyCheckinModel.find();
-        res.status(200).json({ message: "Boats retrieved successfully", data: Checkins, status: true });
+        res.status(200).json({ message: "dailyCheckins retrieved successfully", data: Checkins, status: true });
     } catch (error) {
-        res.status(500).json({ error: 'Could not retrieve boats', status: false });
+        res.status(500).json({ error: 'Could not retrieve dailyCheckins data', status: false });
     }
 });
 
